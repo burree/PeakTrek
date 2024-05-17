368868a162befdbda7245f738c8e0378d641d04e
burree <edwin.andersson1@skola.taby.se>
2024-05-17 21:15:15 +0200

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,20 @@ mapboxgl.accessToken =
       const monument = [18.062978, 59.439965];
       const map = new mapboxgl.Map({
         container: "map",
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: "mapbox://styles/mapbox/light-v11",
         center: monument,
         zoom: 15,
       });
 
-      // create the popup
       const popup = new mapboxgl.Popup({ offset: 25 }).setText(
         "Åva gymnasium"
       );
 
-      // create DOM element for the marker
       const el = document.createElement("div");
       el.id = "marker";
 
-      // create the marker
+      
       new mapboxgl.Marker(el)
         .setLngLat(monument)
-        .setPopup(popup) // sets a popup on this marker
+        .setPopup(popup) 
         .addTo(map);
